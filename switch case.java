18ef7b7ad5847af9b3ee419fe84e442a8ff048cb
KISHAN KUMAR SURESH KUMAR <144879299+Kishankumar1328@users.noreply.github.com>
2024-01-07 07:17:32 +0530

    Add files via upload

---
+++ switch case.java
@@ -0,0 +1,23 @@
+class Main{
+    public static void main(String[]args){
+        int a=16;
+        switch(a){
+            case 10:
+                System.out.println("Value is 10");break;
+            case 11:
+                System.out.println("Value is 11");break;
+            case 12:
+                System.out.println("Value is 12");break;
+            case 13:
+                System.out.println("Value is 13");break;
+            case 14:
+                System.out.println("Value is 14");break;
+            case 15:
+                System.out.println("Value is 15");break;
+            case 16:
+                System.out.println("Value is 16");break;
+            default:
+            System.out.println("Unknown Value");
+        }
+    }
+}
